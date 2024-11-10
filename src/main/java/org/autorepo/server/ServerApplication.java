f11d791360bb22c4135584605040704814495186
bbabbi <mooncheayoung@gmail.com>
2024-11-10 22:13:45 +0900

    #1 [INIT] : SpringBoot 프로젝트 세팅

---
+++ src/main/java/org/autorepo/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package org.autorepo.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
