dc677cf5c407ae1c85717d9296aead0956581061
bbabbi <mooncheayoung@gmail.com>
2024-11-12 01:14:03 +0900

    #1 [FEAT] : ConflictException 구현

---
+++ src/main/java/org/autorepo/server/global/error/exception/ConflictException.java
@@ -0,0 +1,13 @@
+package org.autorepo.server.global.error.exception;
+
+import org.autorepo.server.global.error.ErrorCode;
+
+public class ConflictException extends BusinessException {
+    public ConflictException() {
+        super(ErrorCode.CONFLICT);
+    }
+
+    public ConflictException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
