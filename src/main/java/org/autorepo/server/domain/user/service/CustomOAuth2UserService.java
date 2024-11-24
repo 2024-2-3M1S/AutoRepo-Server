9062dcebc29e3682a43e1f3cf032a8d40e29163b
bbabbi <mooncheayoung@gmail.com>
2024-11-25 05:33:13 +0900

    #4 [CHORE] RequiredArgsConstructor 어노테이션 추가 및 코드 구현 방식 변경

--- src/main/java/org/autorepo/server/domain/user/service/CustomOAuth2UserService.java
+++ src/main/java/org/autorepo/server/domain/user/service/CustomOAuth2UserService.java
@@ -1,5 +1,6 @@
 package org.autorepo.server.domain.user.service;
 
+import lombok.RequiredArgsConstructor;
 import org.autorepo.server.domain.user.entity.User;
 import org.autorepo.server.domain.user.entity.UserRole;
 import org.autorepo.server.domain.user.repository.UserRepository;
@@ -10,32 +11,26 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     private final UserRepository userRepository;
-
-    public CustomOAuth2UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) {
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
         // GitHub의 email 필드를 가져오되, null이면 login으로 대체
-        String githubEmail = oAuth2User.getAttribute("email");
-        if (githubEmail == null) {
-            githubEmail = oAuth2User.getAttribute("login");
-        }
+        String email = oAuth2User.getAttribute("email");
+        final String githubEmail = email != null ? email : oAuth2User.getAttribute("login");
 
-        User user = userRepository.findByGithubId(githubEmail).orElse(null);
-        if (user == null) {
-            user = User.builder()
+        User user = userRepository.findByGithubId(githubEmail).orElseGet(() -> {
+            User newUser = User.builder()
                     .githubId(githubEmail)
                     .userRole(UserRole.USER)
                     .build();
-            userRepository.save(user);
-        }
+            return userRepository.save(newUser);
+        });
 
         return new DefaultOAuth2User(oAuth2User.getAuthorities(), oAuth2User.getAttributes(), "id");
     }
