e3f87ec34260e9fb00977e3ef529d7b1537a25eb
bbabbi <mooncheayoung@gmail.com>
2024-11-25 05:35:34 +0900

    #4 [FEAT] Github 회원가입 및 로그인 - JWT & REDIS 적용

---
+++ src/main/java/org/autorepo/server/global/common/jwt/JwtTokenProvider.java
@@ -0,0 +1,70 @@
+package org.autorepo.server.global.common.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    private static final String USER_ID = "userId";
+    private static final Long ACCESS_TOKEN_EXPIRATION_TIME = 60 * 60 * 1000L; // 1시간
+    private static final Long REFRESH_TOKEN_EXPIRATION_TIME = 7 * 24 * 60 * 60 * 1000L; // 7일
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    public String issueAccessToken(Long userId) {
+        return generateToken(userId, ACCESS_TOKEN_EXPIRATION_TIME);
+    }
+    public String issueRefreshToken(Long userId) {
+        return generateToken(userId, REFRESH_TOKEN_EXPIRATION_TIME);
+    }
+
+    private String generateToken(Long userId, Long expirationTime) {
+        Claims claims = Jwts.claims()
+                .setSubject(userId.toString())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime));
+        return Jwts.builder()
+                .setClaims(claims)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Base64.getEncoder().encode(jwtSecret.getBytes());
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public Long getUserIdFromToken(String token) {
+        return Long.parseLong(Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject());
+    }
+}
