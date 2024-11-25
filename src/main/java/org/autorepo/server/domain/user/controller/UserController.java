0a1fdbd60e96b54e380a9cf5c9d9ac8260971c5c
hysong4u <hayeon_song@naver.com>
2024-11-25 21:07:52 +0900

    #14 [feat] 임시토큰발급 api

--- src/main/java/org/autorepo/server/domain/user/controller/UserController.java
+++ src/main/java/org/autorepo/server/domain/user/controller/UserController.java
@@ -1,16 +1,16 @@
 package org.autorepo.server.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.autorepo.server.domain.token.dto.TokenResponse;
 import org.autorepo.server.domain.user.entity.User;
 import org.autorepo.server.domain.user.repository.UserRepository;
 import org.autorepo.server.global.common.SuccessResponse;
+import org.autorepo.server.global.common.auth.jwt.JwtTokenProvider;
 import org.autorepo.server.global.error.ErrorCode;
 import org.autorepo.server.global.error.exception.BusinessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+
     @GetMapping("/info")
     public ResponseEntity<?> getUserInfo(Authentication authentication) {
         Long userId = (Long) authentication.getPrincipal();
@@ -25,4 +27,13 @@ public class UserController {
                 -> new BusinessException(ErrorCode.USER_NOT_FOUND));
         return ResponseEntity.ok(SuccessResponse.ok(user));
     }
+
+    // 임시 토큰 발급 API 입니다. 추후 로그인 기능이 완성되면 삭제할 예정입니다
+    @PostMapping("/token/{userId}")
+    public ResponseEntity<SuccessResponse<?>> getToken(@PathVariable Long userId) {
+        String accessToken = jwtTokenProvider.issueAccessToken(userId);
+        String refreshToken = jwtTokenProvider.issueRefreshToken(userId);
+        return SuccessResponse.created(TokenResponse.of(accessToken, refreshToken));
+    }
+
 }
