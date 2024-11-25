a26b01c9ad3d61035deb2fac6025b48f096943e5
hysong4u <hayeon_song@naver.com>
2024-11-25 21:14:24 +0900

    #14 [fix] WHITE LIST 제외한 요청에 인증 필요 설정 추가

--- src/main/java/org/autorepo/server/global/config/SecurityConfig.java
+++ src/main/java/org/autorepo/server/global/config/SecurityConfig.java
@@ -4,12 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.autorepo.server.domain.user.service.CustomOAuth2UserService;
 import org.autorepo.server.domain.user.service.OAuth2FailureHandler;
 import org.autorepo.server.domain.user.service.OAuth2SuccessHandler;
-import org.autorepo.server.global.common.jwt.JwtAuthenticationFilter;
+import org.autorepo.server.global.common.auth.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -22,18 +21,17 @@ public class SecurityConfig {
     private final OAuth2FailureHandler oAuth2FailureHandler;
     private final CustomOAuth2UserService customOAuth2UserService;
 
-    private static final String[] WHITE_LIST = {"/api/user", "/api/token/refresh"};
+    private static final String[] WHITE_LIST = {"/api/user/**", "/api/token/refresh"};
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/template/**").permitAll() // 토큰 없이 임시 허용
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/favicon.ico").permitAll()
                         .requestMatchers(WHITE_LIST).permitAll()
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated() // 그 외의 가
                 )
                 .oauth2Login(oauth2 -> {
                     oauth2
