b672d1bb6e57f7e9ac1516ea6891d79375f3c4ec
bbabbi <mooncheayoung@gmail.com>
2024-12-02 01:35:43 +0900

    [Fㅑㅌ] SecurityConfig - CORS 허용

--- src/main/java/org/autorepo/server/global/config/SecurityConfig.java
+++ src/main/java/org/autorepo/server/global/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configure(http))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/favicon.ico").permitAll()
